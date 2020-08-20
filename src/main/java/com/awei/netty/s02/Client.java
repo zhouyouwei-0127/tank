c0706e1230508cd2ecadbfd24e0160db329c28b1
awei <zyw01271314@163.com>
2020-08-20 22:24:09 +0800

    多人聊天功能的实现，client、server UI的编写

---
+++ src/main/java/com/awei/netty/s02/Client.java
@@ -0,0 +1,70 @@
+package com.awei.netty.s02;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class Client {
+
+    //定义写数据的channel
+    Channel channel;
+
+    public void connect() {
+        EventLoopGroup group = new NioEventLoopGroup();
+        Bootstrap b = new Bootstrap();
+        try {
+            b.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline pl = ch.pipeline();
+                            pl.addLast(new ClientHandler());
+                        }
+                    })
+                    .connect("localhost", 8888)
+                    .addListener(new ChannelFutureListener() {
+                        @Override
+                        public void operationComplete(ChannelFuture future) throws Exception {
+                            if (future.isSuccess()) {
+                                System.out.println("connected");
+                                channel = future.channel();
+                            } else {
+                                System.out.println("not connected");
+                            }
+                        }
+                    })
+                    .channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+    public void sendMsg(String msg) {
+        ByteBuf buf = Unpooled.copiedBuffer(msg.getBytes());
+        channel.writeAndFlush(buf);
+    }
+
+    public void closeConnect() {
+        sendMsg("_bye_");
+    }
+}
+
+class ClientHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        byte[] bytes = new byte[buf.readableBytes()];
+        buf.getBytes(buf.readerIndex(), bytes);
+        String str = new String(bytes);
+        //在窗口回显出这条数据
+        ClientFrame.INSTANCE.updateText(str);
+    }
+}
--- src/main/java/com/awei/netty/s02/ClientFrame.java
+++ src/main/java/com/awei/netty/s02/ClientFrame.java
@@ -3,15 +3,30 @@ package com.awei.netty.s02;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class ClientFrame extends Frame {
 
-    TextArea ta = new TextArea();
-    TextField tf = new TextField();
+    //用于Client使用实例调用TextArea回显数据
+    public static final ClientFrame INSTANCE = new ClientFrame();
+
+    //用于连接
+    Client client = null;
+
+    public TextArea ta = new TextArea();
+    public TextField tf = new TextField();
 
     public ClientFrame() {
         this.setSize(600,400);
         this.setLocation(100,20);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                client.closeConnect();
+                System.exit(0);
+            }
+        });
 
         this.add(ta,BorderLayout.CENTER);
         this.add(tf,BorderLayout.SOUTH);
@@ -19,15 +34,24 @@ public class ClientFrame extends Frame {
         tf.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                ta.setText(ta.getText() + tf.getText());
+                client.sendMsg(tf.getText());
                 tf.setText("");
             }
         });
+    }
 
-        this.setVisible(true);
+    public void updateText(String msgAccepted) {
+        this.ta.setText(ta.getText() + System.getProperty("line.separator") + msgAccepted);
+    }
+
+    private void connectToServer() {
+        client = new Client();
+        client.connect();
     }
 
     public static void main(String[] args) {
-        new ClientFrame();
+        ClientFrame frame = ClientFrame.INSTANCE;
+        frame.setVisible(true);
+        frame.connectToServer();
     }
 }
