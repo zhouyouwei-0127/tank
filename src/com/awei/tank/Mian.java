7edef725a5611792a7151e4a9cc3c71d651c0d8a
awei <zyw01271314@163.com>
2020-08-22 22:34:09 +0800

    去掉敌方坦克的初始化，为网络版做好准备

--- src/com/awei/tank/Mian.java
+++ src/com/awei/tank/Mian.java
@@ -7,9 +7,9 @@ public class Mian {
         TankFrame tf = new TankFrame();
         int initTankCount = Integer.parseInt((String)PropertiesMgr.get("initTankCount"));
         //画敌方坦克
-        for (int i = 0; i < initTankCount; i++) {
+        /*for (int i = 0; i < initTankCount; i++) {
             tf.tanks.add(new Tank(50 + 50*i,300,Dir.DOWN,Group.BAD,tf));
-        }
+        }*/
 
         while (true) {
             Thread.sleep(50);
